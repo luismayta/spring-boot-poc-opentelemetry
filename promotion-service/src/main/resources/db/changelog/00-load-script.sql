0001c7a5eda93bb73c70f338742eeafeb6a5a546
Luis Mayta <luis@hadenlabs.com>
2024-06-22 19:54:16 -0300

    fix: clean configuration for liquibase (#2)

--- promotion-service/src/main/resources/db/changelog/00-load-script.sql
+++
@@ -1,4 +0,0 @@
--- Add script dependencies here if they were not added on their respective script.
-
--- This script is intended to be ran every time a change in the file happens. So there is no need to create another
--- script for every dependency found
